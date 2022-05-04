598e0db28d505e358e512738f38b6160685abf14
Anton Kozhukhar <anton.kozhukhar@gmail.com>
2022-05-04 14:31:43 +0300

    Add base url for axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,46 +6,46 @@ export default {
     titleTemplate: '%s - OnLibFan',
     title: 'OnLibFan',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
+  
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
+  
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
+  
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
+  
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    '@nuxtjs/vuetify'
   ],
-
+  
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    '@nuxtjs/axios'
   ],
-
+  
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://localhost:3000/api/'
   },
-
+  
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
@@ -55,12 +55,12 @@ export default {
           primary: colors.purple,
           secondary: colors.grey.darken1,
           accent: colors.shades.black,
-          error: colors.red.accent3,
-        },
-      },
-    },
+          error: colors.red.accent3
+        }
+      }
+    }
   },
-
+  
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {}
 }
