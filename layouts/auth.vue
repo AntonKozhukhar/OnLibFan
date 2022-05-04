5265c62a4ef09ce4923238d9d352908eb7859166
prusichka <denchuk.prusak@gmail.com>
2022-05-04 16:23:48 +0300

    Fix

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar fixed app>
+    <v-app-bar src='https://about.proquest.com/globalassets/proquest/media/images/decrotive/oldbooks.jpg' fixed app>
       <v-toolbar-title v-text='title' />
       <v-spacer></v-spacer>
       <v-btn
         icon
         to='/'
+        color='white'
       >
         <v-icon>mdi-export</v-icon>
       </v-btn>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    Home
-  </div>
+  <v-card class='d-flex flex-wrap pa-3 justify-space-between'>
+    <v-card
+      v-for='(book, index) in books'
+      :key='book.id'
+      :id='index'
+      max-width='344'
+      class='mx-auto mb-4'
+    >
+      <v-img
+        src='https://cdn.vuetifyjs.com/images/cards/sunshine.jpg'
+        height='200px'
+      ></v-img>
+
+      <v-card-title>
+        {{book.title}}
+      </v-card-title>
+
+      <v-card-subtitle>
+        1,000 miles of wonder
+      </v-card-subtitle>
+
+      <v-spacer></v-spacer>
+
+      <v-card-text>
+        {{book.preview}}
+      </v-card-text>
+    </v-card>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data: () => ({
+    books: [
+      {
+        id: 1,
+        title: 'Test1',
+        preview: 'The plugins directory contains JavaScript plugins that you want to run before instantiating the root. Every time you need to use `Vue.use()`, you should create a file in `plugins/` and add its path to plugins in `nuxt.config.js More information about the usage of this directory in',
+      },
+      {
+        id: 2,
+        title: 'Test2',
+        preview: 'The plugins directory contains JavaScript plugins that you want to run before instantiating the root. Every time you need to use `Vue.use()`, you should create a file in `plugins/` and add its path to plugins in `nuxt.config.js More information about the usage of this directory in',
+      },
+      {
+        id: 3,
+        title: 'Test3',
+        preview: 'The plugins directory contains JavaScript plugins that you want to run before instantiating the root. Every time you need to use `Vue.use()`, you should create a file in `plugins/` and add its path to plugins in `nuxt.config.js More information about the usage of this directory in',
+      },
+      {
+        id: 4,
+        title: 'Test4',
+        preview: 'The plugins directory contains JavaScript plugins that you want to run before instantiating the root. Every time you need to use `Vue.use()`, you should create a file in `plugins/` and add its path to plugins in `nuxt.config.js More information about the usage of this directory in',
+      },
+    ],
+    show: false
+  }),
 }
 </script>
